469bcc8d47bedb59bae10051b0444c5d3c3391fb
Luckkk1 <dldbstjr4444@gmail.com>
2022-02-25 10:02:55 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// navbar 상단 픽스
 const navbar = document.querySelector("#navBar");
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -9,4 +10,20 @@ document.addEventListener("scroll", () => {
   } else {
     navbar.classList.remove("navbar--dark");
   }
+});
+
+//메뉴 바로가기
+const navbarMenu = document.querySelector(".navBar__menuBtns");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "center",
+  });
 });
